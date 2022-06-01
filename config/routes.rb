8f75b1e03dfc855bcb0833ad5b955b894b8fa1b8
Greg Berrigan <gberrigan@covermymeds.com>
2022-06-01 13:57:09 -0400

    created product controller and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
+resources :products
 get '/register', to: 'users#new'
-resources :users, only: [:create, :index, :new]
 get '/users/sign_in', to: 'sessions#new', as: :sign_in
 delete '/users/sign_out', to: 'sessions#destroy', as: :sign_out
 resources :sessions, only: [:create]
-
+resources :users
 
   
 end
