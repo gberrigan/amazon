15f19d6e2c2b71d1fe29177c0eac630a6a9ba8c4
Greg Berrigan <gberrigan@covermymeds.com>
2022-06-12 23:34:28 -0400

    added validation to checkout form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,24 @@
 class Order < ApplicationRecord
     
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+
+    validates :email, presence: true
+    validates :address, presence: true
+    validates :country, presence: true
+    validates :state, presence: true, length: {is: 2}
+    validates :zipcode, presence: true, numericality: { only_integer: true }
+
+    #CC information
+    validates :payment_type, presence: true
+    validates :ccname, presence: true
+    validates :ccnumber, presence: true, length: {is: 16}, numericality: { only_integer: true } 
+    validates :expiration, presence: true
+    validates :cvv, presence: true, length: {is: 3}, numericality: { only_integer: true }
+
+    
+    
+
     belongs_to :user
     
 
