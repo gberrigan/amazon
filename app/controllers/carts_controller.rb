5c6c4deb62e1c13f9e1f4cd7c860adea3011898a
Greg Berrigan <gberrigan@covermymeds.com>
2022-06-14 16:07:39 -0400

    added alerts to add_to_cart action

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,12 +11,22 @@ class CartsController < ApplicationController
     def add_to_cart
     cart_item = CartItem.find_by(user: current_user, product_id: params.require(:product_id))
       if cart_item
+        if current_user.member_plus
+          cart_item.shipping_option_id = 1
+        else
+          cart_item.shipping_option_id = 4
+        end
         cart_item.quantity += 1
         cart_item.save
         flash[:notice] = "#{cart_item.name} has successfully been added to your cart (#{cart_item.quantity})"
         redirect_to products_path 
       else
         cart_item = CartItem.new(user: current_user, product_id: params.require(:product_id), quantity: 1)
+        if current_user.member_plus
+          cart_item.shipping_option_id = 1
+        else
+          cart_item.shipping_option_id = 4
+        end
         cart_item.save
         flash[:notice] = "#{cart_item.name} has successfully been added to your cart"
         redirect_to products_path 
@@ -27,14 +37,13 @@ class CartsController < ApplicationController
         @cart_item = CartItem.find(params[:id])
         if params[:cart_item][:quantity].to_i == 0
           @cart_item.destroy
+          flash[:notice] = "#{cart_item.name} has successfully been removed from your cart"
           redirect_to confirm_cart_path
-          #notice Item successfully removed
         else 
-          # if @cart_item.update(cart_item_params)
           if @cart_item.update(quantity: params[:cart_item][:quantity])
+            flash[:notice] = "#{@cart_item.name} has successfully been updated"
             redirect_to confirm_cart_path
           else
-            #notice Item successfully updated
           end
         end
     end
@@ -45,4 +54,11 @@ private
       params.require(:cart_item).permit(:id, :quantity)
     end
 
+    def choose_shipping
+      if current_user.member_plus
+        cart_item.shipping_option_id = 1
+      else
+        cart_item.shipping_option_id = 4
+      end
+    end
 end
