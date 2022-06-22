32b596ecca0fa418208f272f5bc7958424943d3f
Greg Berrigan <gberrigan@covermymeds.com>
2022-06-22 16:08:56 -0400

    Sessions controller update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,11 @@ class SessionsController < ApplicationController
  
   def create
     @user = User.find_by(username: params[:username])
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      render :new
+      redirect_to sign_in_path, notice: 'Username or password is incorrect'
     end
   end
  
